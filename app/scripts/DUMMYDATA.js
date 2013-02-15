8116e6125039413090396d1628049dcc257f0bf6
Matt <hello@mattnull.com>
2013-02-15 13:24:41 -0700

    added rough venue slide

--- app/scripts/DUMMYDATA.js
+++ app/scripts/DUMMYDATA.js
@@ -3,12 +3,179 @@ DUMMYDATA = {
 	id : 'AGDASG12512t32egsdh124gsdh21',
 	slides : [
 		{
-			type : 'updates',
-			title : 'Updates'
-		},
-		{
 			type : 'image',
 			url : 'http://1.bp.blogspot.com/-qddG9U2FWPM/URCbNjGeumI/AAAAAAAAGh8/EImXwchgfYk/s640/tycho-sxsw-20121.jpg'
+		},
+		{
+			type : 'schedule',
+			venue : {
+				street_address: "500 E Cesar Chavez St",
+				sub_category: "Distribution",
+				title: "Austin Convention Center",
+				venue_alpha: "Austin Convention Center",
+				lat: 30.2637792,
+				long: -97.7405461,
+				map : "http://images.statemaster.com/images/motw/historical/austin21.jpg"
+			},
+			schedule: {
+				"9:30 AM" : {
+					_key : "9:30 AM",
+					events : [{
+						id: "FP10773",
+						branded_name: "",
+						category: "Workshops",
+						conference: "Film",
+						date: "2013/03/09",
+						date_endtime_calc: "2013/03/09 12:00:00 -0600",
+						date_starttime_calc: "2013/03/09 11:00:00 -0600",
+						description: "In the age of digital distribution and new marketplaces, we’ll discuss what filmmakers can learn from retail best practices. From building sales relationships, considering shelf space positioning, planning release windows, and activating consumer data, we'll explore why the crossover between the retail world and indie film is more relevant than ever to filmmakers, distributors, and marketers.",
+						end_time: "12:00:00",
+						event_name: "Online Marketplaces: What Filmmakers Need To Know",
+						event_type: "Sessions",
+						experience_level: "Intermediate",
+						formatted_date: "Saturday, March 9",
+						formatted_endtime: "12:00PM",
+						formatted_time: "11:00AM",
+						formatted_time_to_endtime: "11:00AM -12:00PM",
+						hash_tags: "#FilmOnline",
+						image_filename: "",
+						key: "Online Marketplaces: What Filmmakers Need To Know",
+						lat: 30.2637792,
+						long: -97.7405461,
+						name: "Online Marketplaces: What Filmmakers Need To Know",
+						online_ok: "1",
+						panel_type: "",
+						presentation_url: "",
+						presenters: [
+							{
+								status: "Confirmed",
+								presenter_name: "Marc Schiller",
+								presenter_firstname: "Marc",
+								presenter_lastname: "Schiller",
+								presenter_title: "CEO",
+								presenter_company: "BOND Strategy & Influence",
+								moderator: "0",
+								presenter_bio: null,
+								presenter_headshot_url: ""
+							},
+							{
+								status: "Confirmed",
+								presenter_name: "Tom Quinn",
+								presenter_firstname: "Tom",
+								presenter_lastname: "Quinn",
+								presenter_title: "Co-Pres",
+								presenter_company: "RADiUS-TWC",
+								moderator: "0",
+								presenter_bio: null,
+								presenter_headshot_url: ""
+							}
+						]
+					},
+					{
+						key: "9:30AM",
+						id: "FP10773",
+						branded_name: "",
+						category: "Workshops",
+						conference: "Film",
+						date: "2013/03/09",
+						date_endtime_calc: "2013/03/09 12:00:00 -0600",
+						date_starttime_calc: "2013/03/09 11:00:00 -0600",
+						description: "In the age of digital distribution and new marketplaces, we’ll discuss what filmmakers can learn from retail best practices. From building sales relationships, considering shelf space positioning, planning release windows, and activating consumer data, we'll explore why the crossover between the retail world and indie film is more relevant than ever to filmmakers, distributors, and marketers.",
+						end_time: "12:00:00",
+						event_name: "Online Marketplaces: What Filmmakers Need To Know",
+						event_type: "Sessions",
+						experience_level: "Intermediate",
+						formatted_date: "Saturday, March 9",
+						formatted_endtime: "12:00PM",
+						formatted_time: "11:00AM",
+						formatted_time_to_endtime: "11:00AM -12:00PM",
+						hash_tags: "#FilmOnline",
+						image_filename: "",
+						key: "Online Marketplaces: What Filmmakers Need To Know",
+						lat: 30.2637792,
+						long: -97.7405461,
+						name: "Online Marketplaces: What Filmmakers Need To Know",
+						online_ok: "1",
+						panel_type: "",
+						presentation_url: "",
+						presenters: [
+							{
+								status: "Confirmed",
+								presenter_name: "Marc Schiller",
+								presenter_firstname: "Marc",
+								presenter_lastname: "Schiller",
+								presenter_title: "CEO",
+								presenter_company: "BOND Strategy & Influence",
+								moderator: "0",
+								presenter_bio: null,
+								presenter_headshot_url: ""
+							},
+							{
+								status: "Confirmed",
+								presenter_name: "Tom Quinn",
+								presenter_firstname: "Tom",
+								presenter_lastname: "Quinn",
+								presenter_title: "Co-Pres",
+								presenter_company: "RADiUS-TWC",
+								moderator: "0",
+								presenter_bio: null,
+								presenter_headshot_url: ""
+							}
+						]
+					},
+					{
+						key: "9:30AM",
+						id: "FP10773",
+						branded_name: "",
+						category: "Workshops",
+						conference: "Film",
+						date: "2013/03/09",
+						date_endtime_calc: "2013/03/09 12:00:00 -0600",
+						date_starttime_calc: "2013/03/09 11:00:00 -0600",
+						description: "In the age of digital distribution and new marketplaces, we’ll discuss what filmmakers can learn from retail best practices. From building sales relationships, considering shelf space positioning, planning release windows, and activating consumer data, we'll explore why the crossover between the retail world and indie film is more relevant than ever to filmmakers, distributors, and marketers.",
+						end_time: "12:00:00",
+						event_name: "Online Marketplaces: What Filmmakers Need To Know",
+						event_type: "Sessions",
+						experience_level: "Intermediate",
+						formatted_date: "Saturday, March 9",
+						formatted_endtime: "12:00PM",
+						formatted_time: "11:00AM",
+						formatted_time_to_endtime: "11:00AM -12:00PM",
+						hash_tags: "#FilmOnline",
+						image_filename: "",
+						key: "Online Marketplaces: What Filmmakers Need To Know",
+						name: "Online Marketplaces: What Filmmakers Need To Know",
+						online_ok: "1",
+						panel_type: "",
+						presentation_url: "",
+						presenters: [
+							{
+								status: "Confirmed",
+								presenter_name: "Marc Schiller",
+								presenter_firstname: "Marc",
+								presenter_lastname: "Schiller",
+								presenter_title: "CEO",
+								presenter_company: "BOND Strategy & Influence",
+								moderator: "0",
+								presenter_bio: null,
+								presenter_headshot_url: ""
+							},
+							{
+								status: "Confirmed",
+								presenter_name: "Tom Quinn",
+								presenter_firstname: "Tom",
+								presenter_lastname: "Quinn",
+								presenter_title: "Co-Pres",
+								presenter_company: "RADiUS-TWC",
+								moderator: "0",
+								presenter_bio: null,
+								presenter_headshot_url: ""
+							}
+						]
+					}]
+				}
+			}
+
 		},
 		{
 			type : 'url',
@@ -19,74 +186,8 @@ DUMMYDATA = {
 			url : 'http://goodattheinternet.com'
 		},
 		{
-			type : 'url',
-			url : 'http://zachherring.com'
+			type : 'updates',
+			title : 'Updates'
 		},
-		{
-			type : 'schedule',
-			title : 'Schedule',
-			id: "event_FP10773",
-			key: "event_FP10773",
-			value: {
-				id: "FP10773",
-				branded_name: "",
-				category: "Workshops",
-				conference: "Film",
-				date: "2013/03/09",
-				date_endtime_calc: "2013/03/09 12:00:00 -0600",
-				date_starttime_calc: "2013/03/09 11:00:00 -0600",
-				description: "In the age of digital distribution and new marketplaces, we’ll discuss what filmmakers can learn from retail best practices. From building sales relationships, considering shelf space positioning, planning release windows, and activating consumer data, we'll explore why the crossover between the retail world and indie film is more relevant than ever to filmmakers, distributors, and marketers.",
-				end_time: "12:00:00",
-				event_name: "Online Marketplaces: What Filmmakers Need To Know",
-				event_type: "Sessions",
-				experience_level: "Intermediate",
-				formatted_date: "Saturday, March 9",
-				formatted_endtime: "12:00PM",
-				formatted_time: "11:00AM",
-				formatted_time_to_endtime: "11:00AM -12:00PM",
-				hash_tags: "#FilmOnline",
-				image_filename: "",
-				key: "Online Marketplaces: What Filmmakers Need To Know",
-				lat: 30.2637792,
-				long: -97.7405461,
-				name: "Online Marketplaces: What Filmmakers Need To Know",
-				online_ok: "1",
-				panel_type: "",
-				presentation_url: "",
-				presenters: [
-					{
-						status: "Confirmed",
-						presenter_name: "Marc Schiller",
-						presenter_firstname: "Marc",
-						presenter_lastname: "Schiller",
-						presenter_title: "CEO",
-						presenter_company: "BOND Strategy & Influence",
-						moderator: "0",
-						presenter_bio: null,
-						presenter_headshot_url: ""
-					},
-					{
-						status: "Confirmed",
-						presenter_name: "Tom Quinn",
-						presenter_firstname: "Tom",
-						presenter_lastname: "Quinn",
-						presenter_title: "Co-Pres",
-						presenter_company: "RADiUS-TWC",
-						moderator: "0",
-						presenter_bio: null,
-						presenter_headshot_url: ""
-					}
-				],
-				room: "Room 13AB",
-				start_time: "11:00:00",
-				street_address: "500 E Cesar Chavez St",
-				sub_category: "Distribution",
-				theme: "Distribution",
-				type: "event",
-				venue: "Austin Convention Center",
-				venue_alpha: "Austin Convention Center"
-			}
-
-		}
 	]
 }
